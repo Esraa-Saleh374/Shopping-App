2ab64731a1c487117c7592fe60d7d0b52333d4eb
Kareem Allam <Kareem.i.Allam@gmail.com>
2021-08-12 19:58:00 +0200

    .gitignore fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/config/const/theme_data.dart';
+import 'package:shopping/providers/darkmode_theme.dart';
 
 void main() {
-  // SystemChrome.setEnabledSystemUIOverlays([]); // Hide Notif. Toolbar
-  // SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values); // Show Notif. Toolbar
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeProvider = DarkThemeProvider();
+  void getCurrentTheme() async {
+    themeProvider.darkTheme =
+        await themeProvider.darkThemePreferences.getTheme();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: ' ECommerce',
+      theme: Styles.themeData(themeProvider.darkTheme, context),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to Flutter'),
